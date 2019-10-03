36c1a994e2e45d8c30dea765b230368e3834ba77
martinVollenweider <martin.vollenweider@htwchur.ch>
2019-10-03 13:43:19 +0200

    Used latest version of subjx.js

--- showObject.php
+++ showObject.php
@@ -3,12 +3,16 @@
 	ini_set('display_errors', '1');
 
 	include_once('system/data.php');
-	//include_once('system/security.php');
 
 	// Funktion in data.php
 	$result = show_content();
 
 	while($event = mysqli_fetch_assoc($result)){
+		// Mögliche leere Texte ersetzen
+		if ($event['cont'] == '') {
+			$event['cont'] = "Hier Text, bitte ersetzen";
+		}
+		
 		$out = "<div contenteditable='true' id='id_".$event['id']."' class='draggable' ";
 		$out.= "style='top:".$event['y']."px; left:".$event['x']."px; transform:rotate(".$event['rot']."deg); ";
 		$out.= "width:".$event['w']."px; height:".$event['h']."px;'>";
